3ae37c56f5844e30756a3c7446761147b4f3f6ae
AhmedShab <ahmed.vuw@gmail.com>
2016-05-28 15:28:20 +1200

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  context: __dirname,
+  entry: "./src/js/entry.js",
+
+  output: {
+    path: __dirname + "/src/js",
+    filename: "bundle.js"
+  },
+
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+        query: {
+          presets: ['react', 'es2015']
+        }
+      }
+    ]
+  }
+};
